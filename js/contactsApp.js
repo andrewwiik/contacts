6e026d03419efc8bdfb6b221208dd3919bc9dee3
Andrew Wiik <root@localhost.localdomain>
2015-06-18 14:02:56 -0400

    fgd

--- js/contactsApp.js
+++ js/contactsApp.js
@@ -2,7 +2,7 @@
 
 var contactsApp = angular.module('contactsApp', []);
     contactsApp.controller('contactController', [ "$scope", "$http", function ($scope, $http){
-      $http({method: 'GET', url: '../contacts.php'}).success(function(data){
+      $http({method: 'GET', url: 'contacts.php'}).success(function(data){
         $scope.contact = data;
       });
     $scope.sortField = 'fname';
