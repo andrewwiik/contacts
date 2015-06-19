c62491687cc4cbdc10930e369d3ea9695c207c39
Andrew Wiik <andrew.wiik@vtsd.com>
2015-06-19 10:08:54 -0400

    Update contactApp.js

--- js/contactApp.js
+++ js/contactApp.js
@@ -2,7 +2,7 @@
 
 var contactApp = angular.module('contactApp', []);
     contactApp.controller('contactController', [ "$scope", "$http", function ($scope, $http){
-      $http({method: 'GET', url: 'contact.php'}).success(function(data){
+      $http({method: 'GET', url: 'contacts.php'}).success(function(data){
         $scope.contact = data;
       });
     $scope.sortField = 'contact';
